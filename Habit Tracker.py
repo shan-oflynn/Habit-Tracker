ecb34cc74e2850e882d7fe852827015eec1f967a
shan-oflynn <shannonoflynn00@gmail.com>
2025-10-13 10:20:16 -0400

    Update habit tracker to include delete function and load user

--- Habit Tracker.py
+++ Habit Tracker.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[39]:
+# In[3]:
 
 
 # Import json to allow saving
 import json
 
-
-# In[1]:
-
-
-# Initializes habits dictionary
-habits = {}
-
-
-# In[19]:
+# Try to reload previously saved data
+try:
+    with open("habits.json", "r") as file:
+        habits = json.load(file)
+except FileNotFoundError:
+     habits = {}         
 
 
-# Ask for user. Cleans input
-user = input("What is your name?").strip().title()
+# In[4]:
 
-# If user isn't already being tracked, adds to dictionary of users
+
+# Ask for user and clean the input
+user = input("Please enter your name: ").strip().title()
+
+# If user isn't already in habits, add them
 if user not in habits:
     habits[user] = {}
 
 
-# In[46]:
+# In[4]:
 
 
 def save_habits():
@@ -34,7 +34,7 @@ def save_habits():
         json.dump(habits, saved_habits, indent = 4)
 
 
-# In[41]:
+# In[5]:
 
 
 def add_habit():
@@ -78,7 +78,7 @@ def add_habit():
         print("Have a nice day")
 
 
-# In[42]:
+# In[6]:
 
 
 def log_habit(user, habit_name = None):
@@ -106,7 +106,7 @@ def log_habit(user, habit_name = None):
             print("Please add a valid habit and try again") # Exit statement telling user to try again
 
 
-# In[5]:
+# In[7]:
 
 
 def view_habits():
@@ -114,7 +114,7 @@ def view_habits():
         print(f"{user}: {habit_name} {info['count']}/{info['frequency']}")
 
 
-# In[37]:
+# In[8]:
 
 
 def view_stats(user):
@@ -143,20 +143,51 @@ def view_stats(user):
         print(f"{habit_name:<15}[{progress_bar}] {count}/{frequency} {progress*100:.1f}%")   
 
 
+# In[9]:
+
+
+def delete_habit(user, habit_name = None):
+    # if no habit name previously inputted, asks for habit
+    if habit_name is None:
+        while True:
+            habit_name = input("What habit would you like to delete?").strip().title()
+            if habit_name == "":
+                print("Entry not valid. Please try again.")
+            else:
+                break # if given a valid input, break loop
+
+# If habit is under inputted user's habits, ask for confirmation to delete; if yes, delete. No, "Have a nice day."
+# If habit not in habits, tell user and ask if they would like to try again.
+    if habit_name in habits[user]:
+        yes_or_no = input(f"Do you wish to delete {habit_name.lower()}?")
+        yes_or_no = yes_or_no.strip().lower()
+        if yes_or_no == "yes":
+            habits[user].pop(habit_name)
+            print(f"You have successfully removed {habit_name.lower()}.")
+        save_habits()
+    else: 
+        yes_or_no = input("Habit has not been logged in my system. Would you like to try again")
+        yes_or_no = yes_or_no.strip().lower()
+        if yes_or_no == "yes":
+            delete_habit(user) # Brings user back to top of delete habit
+        else: 
+            print("No habit has been deleted.") # Exit statement telling user to try again
+
+
 # In[ ]:
 
 
 # ASK USER WHAT THEY WANT TO DO
 
 
-# In[43]:
+# In[37]:
 
 
-print("Please select the number of what you would like to do: \n 1. Add a habit \n 2. Log habit \n 3. View habits \n 4. View stats \n 5. Exit")
+print("Please select the number of what you would like to do: \n 1. Add a habit \n 2. Log habit \n 3. View habits \n 4. View stats \n 5. Delete habit \n 6. Exit")
 selection = input("Select a number")
 
 
-# In[ ]:
+# In[38]:
 
 
 if selection == "1":
@@ -167,6 +198,8 @@ elif selection == "3":
     view_habits()
 elif selection == "4": 
     view_stats(user)
+elif selection == "5":
+    delete_habit(user)
 else:
     print("Have a nice day")
 
